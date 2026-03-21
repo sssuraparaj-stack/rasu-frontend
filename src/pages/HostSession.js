ba36ca1384eeeab0aacfce869ad43cbf71826bbf
Syama Sundar <sssuraparaj@gmail.com>
2026-03-21 14:07:00 +0530

    feat: add QR code to host session

--- src/pages/HostSession.js
+++ src/pages/HostSession.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+Fimport React, { useState, useEffect, useRef } from 'react';
 import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import { io } from 'socket.io-client';
 import { API, WS } from '../App';
